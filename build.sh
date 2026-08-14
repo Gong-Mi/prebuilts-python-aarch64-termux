0ca05abd5d2f81d9c216e131b62ea11d3977679a
Gong-Mi <62661921+Gong-Mi@users.noreply.github.com>
2026-08-14 19:49:36 +0800

    build: exclude all CPython test module prefixes

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ if [[ ! -f Modules/Setup.local ]]; then
   sed -E 's/^#([a-z_*].*)$/\1/p' Modules/Setup \
     | sed -E 's/^[[:space:]]+//' \
     | sed 's/^\*shared\*$/\*static\*/' \
-    | grep -v -E '^(_ctypes|_curses|_curses_panel|_dbm|_gdbm|_lzma|_ssl|_hashopenssl|_hashlib|_sqlite3|_tkinter|_bz2|_zstd|_uuid|_test|xx|readline)([[:space:]]|$)' \
+    | grep -v -E '^(_ctypes|_curses|_curses_panel|_dbm|_gdbm|_lzma|_ssl|_hashopenssl|_hashlib|_sqlite3|_tkinter|_bz2|_zstd|_uuid|_test|xx|readline)' \
     > Modules/Setup.local
 fi
 if [[ ! -f Makefile ]]; then
@@ -80,7 +80,7 @@ fi
 sed -E 's/^#([a-z_*].*)$/\1/p' Modules/Setup \
   | sed -E 's/^[[:space:]]+//' \
   | sed 's/^\*shared\*$/\*static\*/' \
-  | grep -v -E '^(_ctypes|_curses|_curses_panel|_dbm|_gdbm|_lzma|_ssl|_hashopenssl|_hashlib|_sqlite3|_tkinter|_bz2|_zstd|_uuid|_test|xx|readline)([[:space:]]|$)' \
+  | grep -v -E '^(_ctypes|_curses|_curses_panel|_dbm|_gdbm|_lzma|_ssl|_hashopenssl|_hashlib|_sqlite3|_tkinter|_bz2|_zstd|_uuid|_test|xx|readline)' \
   > Modules/Setup.local
 
 # Android/Bionic provides librt APIs through libc and has no standalone librt.
